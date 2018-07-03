2b617b6efa3c3e10d45f074d6cb13bc43a45830e
Tirsa <tr.galvan@gmail.com>
2018-07-03 11:39:17 +0200

    week1 assignment

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -7,9 +7,35 @@ const http = require('http');
  */
 function createServer(port) {
   let state = 10;
+  let currentState = null;
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    if (request.url === '/state') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      let currentState = { 'state': state };
+      response.end(JSON.stringify(currentState));
+    }
+    else if (request.url === '/add') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      let currentState = { 'state': state++ };
+      response.end(JSON.stringify(currentState));
+    }
+    else if (request.url === '/subtract') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      let currentState = { 'state': state-- };
+      response.end(JSON.stringify(currentState));
+    }
+    else if (request.url === '/reset') {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      let state = 10;
+      let currentState = { 'state': state };
+      response.end(JSON.stringify(currentState));
+    }
+    else
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+    let currentState = { 'error': 'Not found' };
+    response.end(JSON.stringify(currentState));
   });
 
   return server;
